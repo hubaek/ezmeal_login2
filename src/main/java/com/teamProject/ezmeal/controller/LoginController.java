3161e09803cb28dc2ad4b1df4ebdc9f84db24606
hubaek <zjfl3122@naver.com>
2023-07-01 03:47:18 +0900

    Feat (login) : login 기능 추가

--- src/main/java/com/teamProject/ezmeal/controller/LoginController.java
+++ src/main/java/com/teamProject/ezmeal/controller/LoginController.java
@@ -1,88 +1,90 @@
 package com.teamProject.ezmeal.controller;
 
-import com.teamProject.ezmeal.dao.MemberDao;
-import com.teamProject.ezmeal.domain.MemberDto;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.teamProject.ezmeal.service.LoginService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import java.lang.reflect.Member;
-import java.net.URLEncoder;
 
 @Controller
-@RequestMapping("/login")
+@RequiredArgsConstructor
 public class LoginController {
-    @Autowired
-    MemberDao memberDao;
+    private final LoginService loginService;
 
     @GetMapping("/login")
-    public String login() {
+    public String getLogin(@RequestParam(defaultValue = "/") String redirectURL, Model model) {
+        // 로그인 후 직전페이지로 가기위한 queryString을 model에 넣어서 login.jsp의 post Action에 쿼리형태로 넣기
+        model.addAttribute("redirectURL", redirectURL);
         return "login";
+    }
+
+    @PostMapping("/login")
+    public String postLogin(HttpServletRequest req,
+                            @RequestParam(defaultValue = "/") String redirectURL,
+                            String tryMbrId,
+                            String tryMbrPw,
+                            Model model
+    ) throws Exception {
+//        // checkbox가 click시 on 아닐 시, null 반환해서 nullPointException 막기 위해 검증
+//        boolean exisitCookie = remember != null;
+//        // cookie 설정하는 logic
+//        // clicked on이면 if문 돌고 아니면 그전에 있던 cookie 제거
+//        if (exisitCookie) {
+//            Cookie cookie = new Cookie("id", tryMbrId);
+//            res.addCookie(cookie);
+//        } else {
+//            Cookie[] cookies = req.getCookies();
+//            for (Cookie removeCookie : cookies) {
+//                if (removeCookie.getName().equals("id")) {
+//                    removeCookie.setMaxAge(0);
+//                    res.addCookie(removeCookie);
+//                    break;
+//                }
+//            }
+//        }
+//        // rememberId cookie logic 끝
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /* 개인 정보를 jsp로 보낼 경우, 특히 redirect를 하는 경우는 조심!!!!!
+         * redirect인 경우는 model로 담은 값들이 url에 그대로 나타나게 된다.
+         * 따라서 session에 값을 넣어서 보내는 방법을 택해야한다.
+         * */
+
+        Long memberId = loginService.getLogin(tryMbrId, tryMbrPw);
+
+        // login 검증
+        // session도 없고 session 있는데 login 정보 없을 때 들어옴
+        if (memberId == 1L ) {
+            model.addAttribute("wrongId", "id가 올바르지 않습니다.");
+            return "login";
+        } else if (memberId == 2L) {
+            model.addAttribute("wrongPw", "pw가 올바르지 않습니다.");
+            return "login";
+        } else {
+            // login성공시 filter에서 구분할 session 넣어주기
+            HttpSession session = req.getSession();
+            session.setAttribute("memberId", memberId);
+            model.addAttribute("checkLoginSuccess", "login success!!");
+            return "redirect:" + redirectURL;
+            // 이제 session에 있는 pk를 이용해서 값을 유지할 수 있다.
+        }
+
     }
 
     @GetMapping("/logout")
     public String logout(HttpSession session) {
-        // 1. 세션을 종료
-        session.invalidate();
-        // 2. 홈으로 이동
+        // logout 기능 수행
+        if (session != null) session.invalidate(); // null 이 아니면 바로 invalidate로 넘기기
         return "redirect:/";
     }
 
-    @PostMapping("/login")
-    public String login(String lgin_id, String lgin_pw, String toURL, boolean rememberId,
-                        HttpServletRequest request, HttpServletResponse response) throws Exception {
-
-        System.out.println("id = " + lgin_id);
-        System.out.println("pwd = " + lgin_pw);
-
-        // 1. lgin_id와 lgin_pw를 확인
-        if (!loginCheck(lgin_id, lgin_pw)) {
-            // 2-1   일치하지 않으면, loginForm으로 이동
-            String msg = URLEncoder.encode("id 또는 pwd가 일치하지 않습니다.", "utf-8");
-
-            return "redirect:/login?msg=" + msg;
-        }
-        // 2-2. lgin_id와 lgin_pw가 일치하면,
-        //  세션 객체를 얻어오기
-        HttpSession session = request.getSession();
-        //  세션 객체에 lgin_id를 저장
-        session.setAttribute("lgin_id", lgin_id);
-
-        if (rememberId) {
-            //     1. 쿠키를 생성
-            Cookie cookie = new Cookie("lgin_id", lgin_id); // ctrl+shift+o 자동 import
-//		       2. 응답에 저장
-            response.addCookie(cookie);
-        } else {
-            // 1. 쿠키를 삭제
-            Cookie cookie = new Cookie("lgin_id", lgin_id); // ctrl+shift+o 자동 import
-            cookie.setMaxAge(0); // 쿠키를 삭제
-//		       2. 응답에 저장
-            response.addCookie(cookie);
-        }
-//		       3. 홈으로 이동
-        toURL = toURL == null || toURL.equals("") ? "/" : toURL;
-
-        return "redirect:" + toURL;
-    }
-
-    private boolean loginCheck(String lgin_id, String lgin_pw) {
-        MemberDto memberDto = null;
-
-        try {
-            memberDto = memberDao.selectMember(lgin_id);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return false;
-        }
-
-        return memberDto != null && memberDto.getLgin_pw().equals(lgin_pw);
-//        return "asdf".equals(lgin_id) && "1234".equals(pwd);
-    }
+    /* Todo
+     * 1. login form 받아와서 변경하기
+     * 2. login pageFilter 처리하고 수행하기
+     * 3. session -> 정보유지 용도, cookie -> rememberId 마무리만 잘 해주기l
+     * */
 }
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -1,16 +1,18 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: hubaek
-  Date: 2023/06/23
-  Time: 3:44 PM
-  To change this template use File | Settings | File Templates.
---%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
-
+</br>
+</br>
+</br>
+<form action="/login?redirectURL=${redirectURL}" method="post">
+    id <input type="text" name="tryMbrId" id="id" value="${cookie.id != null ? cookie.id.value : ''}"> pwd
+    <input type="password" name="tryMbrPw" id="pw"/>
+    <input type="submit" value="login"/>
+    <%--  check 되면 dom property : checked=true  --%>
+    id 기억<input type="checkbox" name="remember" id="remember" ${cookie.id != null ? "checked" : ''}/>
+</form>
 </body>
 </html>
